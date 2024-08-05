429fe1f8d033caae4584ac6d0e5653513d166cb0
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-08-06 00:17:11 +0530

    Add delete button, custom buttons, and closing a modal programmatically

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react"
-import { Text } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
+import IconButton from "../components/UI/IconButton"
+import { GlobalStyles } from "../constants/styles"
 
 const ManageExpense = ({route, navigation}) => {
 
@@ -12,9 +14,66 @@ const ManageExpense = ({route, navigation}) => {
     })
   }, [navigation, isEditing])
 
+  function deleteExpenseHandler() {
+    navigation.goBack()
+  }
+
+  function cancelHandler() {
+    navigation.goBack()
+  }
+
+  function confirmHandler() {
+    navigation.goBack()
+  }
+
   return (
-    <Text>Manage Expense Screen</Text>
+    <View style={styles.container}>
+
+      <View>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+
+        <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+      </View>
+
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton 
+            icon="trash" 
+            color={GlobalStyles.colors.error500} 
+            size={36} 
+            onPress={deleteExpenseHandler} 
+          />
+        </View>
+      )}
+
+    </View>
   )
 }
 
 export default ManageExpense
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center'
+  }
+})
