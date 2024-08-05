e7cd73cdb5533759250a776fdecc617e9a5d81c4
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-08-05 22:27:50 +0530

    Styling Expense Management screen and creating different modes using route parameters

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -3,12 +3,14 @@ import { GlobalStyles } from "../../constants/styles"
 import { getFormattedDate } from "../../util/date"
 import { useNavigation } from "@react-navigation/native"
 
-const ExpenseItem = ({ description, date, amount }) => {
+const ExpenseItem = ({ id, description, date, amount }) => {
 
     const navigation = useNavigation()
 
     function expensePressHandler() {
-        navigation.navigate('ManageExpense')
+        navigation.navigate('ManageExpense', {
+            expenseId: id
+        })
     }
 
   return (
