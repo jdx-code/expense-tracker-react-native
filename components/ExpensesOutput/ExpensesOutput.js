b54b6b5ce2119a7eb4ea8f58dca803c49c4c1d87
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-08-06 13:13:25 +0530

    Managing app-wide state with Context

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,71 +3,14 @@ import ExpensesSummary from "./ExpensesSummary"
 import ExpensesList from "./ExpensesList"
 import { GlobalStyles } from "../../constants/styles"
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 55.90,
-    date: new Date('2021-12-19')
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 87.65,
-    date: new Date('2022-01-05')
-  },
-  {
-    id: 'e3',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-01')
-  },
-  {
-    id: 'e4',
-    description: 'A book',
-    amount: 14.99,
-    date: new Date('2022-02-19')
-  },  
-  {
-    id: 'e5',
-    description: 'Another book',
-    amount: 18.50,
-    date: new Date('2022-02-20')
-  },  
-  {
-    id: 'e6',
-    description: 'Some apples',
-    amount: 7.42,
-    date: new Date('2022-03-09')
-  },
-  {
-    id: 'e7',
-    description: 'A suit',
-    amount: 75.00,
-    date: new Date('2021-12-22')
-  },  
-  {
-    id: 'e8',
-    description: 'A pen',
-    amount: 2.99,
-    date: new Date('2022-01-14')
-  },  
-  {
-    id: 'e9',
-    description: 'Coke',
-    amount: 10.99,
-    date: new Date('2022-03-11')
-  },    
-]
-
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
     <View style={styles.container}>
         <ExpensesSummary
-          expenses={DUMMY_EXPENSES}
+          expenses={expenses}
           periodName={expensesPeriod} />
         <ExpensesList 
-          expenses={DUMMY_EXPENSES} 
+          expenses={expenses} 
         />
     </View>
   )
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,8 +1,20 @@
+import { useContext } from "react"
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput"
+import { ExpensesContext } from "../store/expenses-context"
 
 const RecentExpenses = () => {
+
+  const expensesCtx = useContext(ExpensesContext)
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date()
+    const date7DaysAgo = getDateMinusDays(today, 7)
+
+    return expense.date > date7DaysAgo
+  })
+
   return (
-    <ExpensesOutput expensesPeriod="Last 7 Days" />
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 Days" />
   )
 }
 
